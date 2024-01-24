ea3c6d04204dc8b4e8da07d72e26ca7f61439065
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-01-24 10:39:19 +0600

    create category completed

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -66,7 +66,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="#" class="brand-link">
+            <a href="{{url('admin/dashboard')}}" class="brand-link">
                 <img src="{{asset('admin/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">LARAVEL SHOP</span>
@@ -80,13 +80,13 @@
                         <!-- Add icons to the links using the .nav-icon class
 								with font-awesome or any other icon font library -->
                         <li class="nav-item">
-                            <a href="dashboard.html" class="nav-link">
+                            <a href="{{url('admin/dashboard')}}" class="nav-link">
                                 <i class="nav-icon fas fa-tachometer-alt"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="categories.html" class="nav-link">
+                            <a href="{{url('/admin/categories/create')}}" class="nav-link">
                                 <i class="nav-icon fas fa-file-alt"></i>
                                 <p>Category</p>
                             </a>
