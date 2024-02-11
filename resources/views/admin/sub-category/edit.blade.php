bdd1e08d5d87ecc77c23d2bfbc7e3e0f358984ec
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-02-11 23:56:14 +0600

    btn Update

--- resources/views/admin/sub-category/edit.blade.php
+++ resources/views/admin/sub-category/edit.blade.php
@@ -66,7 +66,7 @@
                 </div>
             </div>
             <div class="pb-5 pt-3">
-                <button type="submit" class="btn btn-primary">Create</button>
+                <button type="submit" class="btn btn-primary">Update</button>
                 <a href="{{route('sub-categories.index')}}" class="btn btn-outline-dark ml-3">Cancel</a>
             </div>
         </form>
