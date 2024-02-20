be5e5b9fb16d6d337ccb5324e5a0556434705102
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-02-21 00:00:00 +0600

    brand view

--- app/Http/Controllers/admin/BrandController.php
+++ app/Http/Controllers/admin/BrandController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class BrandsController extends Controller
+class BrandController extends Controller
 {
     //
 }
