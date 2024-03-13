0e08a275143227e301b0d02df3fd93791eab5bf5
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-03-14 01:12:08 +0600

    Category, Sub category & brand view updated

--- app/Http/Controllers/admin/ProductController.php
+++ app/Http/Controllers/admin/ProductController.php
@@ -3,13 +3,32 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\brand;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function create()
     {
-        return view('admin.product.create');
+        $data = [];
+
+        $categories = Category::orderBy('name', 'ASC')->get();
+        $subCategories = SubCategory::orderBy('name', 'ASC')->get();
+        $brands = brand::orderBy('name', 'ASC')->get();
+
+        $data['subCategories'] = $subCategories;
+        $data['categories'] = $categories;
+        $data['brands'] = $brands;
+
+
+        return view('admin.product.create', $data);
+    }
+
+    public function store()
+    {
+
     }
 
     public function index()
