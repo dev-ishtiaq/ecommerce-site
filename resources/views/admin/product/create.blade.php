2d6c1fd3befb51c74f2ca80fea1117fea964977b
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-03-13 23:58:53 +0600

    view section updated

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -1,12 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<h1>HIII</h1>
-</body>
-</html>
+@extends('admin.layout.app')
+@section('content')
+<!-- Content Header (Page header) -->
+
+@endsection
+
+@section('customjs')
+<script>
+    $("#subCategoryForm").submit(function(event) {
+        event.preventDefault();
+        var element = $(this);
+        $('button[type=submit]').prop('disabled', true);
+        $.ajax({
+            url: '{{route("sub-categories.store")}}',
+            type: 'post',
+            data: element.serializeArray(),
+            dataType: 'json',
+            success: function(response) {
+                $('button[type=submit]').prop('disabled', false);
+                if (response["status"] == true) {
+                    window.location.href = "{{route('sub-categories.create')}}";
+                    $("#name").removeClass('is-invalid')
+                        .siblings('p')
+                        .removeClass('invalid-feedback').html("");
+                    $("#slug").removeClass('is-invalid')
+                        .siblings('p')
+                        .removeClass('invalid-feedback').html("");
+                } else {
+                    var errors = response['errors'];
+                    if (errors['name']) {
+                        $("#name").addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback').html(errors['name']);
+                    } else {
+                        $("#name").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback').html("");
+                    }
+                    if (errors['slug']) {
+                        $("#slug").addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback').html(errors['slug']);
+                    } else {
+                        $("#slug").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback').html("");
+                    }
+                    if (errors['category']) {
+                        $("#category").addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback').html(errors['category']);
+                    } else {
+                        $("#category").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback').html("");
+                    }
+                }
+            },
+            error: function(jqXHR, exception) {
+                console.log("something went wrong");
+            }
+        })
+    })
+
+    // get slug
+    $("#name").change(function() {
+        element = $(this);
+        $('button[type=submit]').prop('disabled', true);
+        $.ajax({
+            url: '{{route("getSlug")}}',
+            type: 'get',
+            data: {
+                title: element.val()
+            },
+            dataType: 'json',
+            success: function(response) {
+                $('button[type=submit]').prop('disabled', false);
+                if (response["status"] == true) {
+                    $("#slug").val(response["slug"]);
+                }
+            }
+        });
+    });
+
+</script>
+@endsection
+
