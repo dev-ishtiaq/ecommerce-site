835548caff7390ea92896a7a12205e7c13cde20b
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-03-15 23:58:02 +0600

    index func

--- app/Http/Controllers/admin/ProductSubCategoryController.php
+++ app/Http/Controllers/admin/ProductSubCategoryController.php
@@ -7,5 +7,14 @@ use Illuminate\Http\Request;
 
 class ProductSubCategoryController extends Controller
 {
-    //
+    public function index (Request $request)
+    {
+        SubCategry::where('category_id', $request->category_id)
+        ->orderBy('name', 'ASC')->get();
+
+        return response()->json([
+            'status' => true,
+            'subCategories' => $subCategories
+        ]);
+    }
 }
