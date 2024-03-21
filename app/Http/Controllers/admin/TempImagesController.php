e9ec5d6879c07585b3912f7f2b4e7436172e12b7
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-03-21 16:51:17 +0600

    intervention issue

--- app/Http/Controllers/admin/TempImagesController.php
+++ app/Http/Controllers/admin/TempImagesController.php
@@ -5,12 +5,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\TempImage;
 use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class TempImagesController extends Controller
 {
    public function create (Request $request)
    {
-      $image = $request->image;
+      $image = $request->file('image');
       if(!empty($image)) {
         $ext = $image->getClientOriginalExtension();
         $newName = time().'.'.$ext;
@@ -23,14 +24,25 @@ class TempImagesController extends Controller
         $image->move(public_path().'/tempImage', $newName);
 
         // Generate thumbnail
-        require 'vendor/autoload.php';
-        $manager = new ImageManager(['driver' => 'imagick']);
+
+        $manager = new ImageManager(new Driver());
+
         $sourcePath = public_path().'/tempImage/'.$newName;
         $destPath = public_path().'/tempImage/thumb/'.$newName;
-        $image = $manager::make($sourcePath);
-        $image->fit(300, 280);
-        
-        $image->save($destPath);
+
+        $img = $manager->read($sourcePath);
+        $img->resize(300, 280);
+        $img->save($destPath);
+
+        // ->toJpeg(80)
+        // $image = $request->file('image');
+
+        // $sourcePath = public_path().'/tempImage/'.$newName;
+        // $destPath = public_path().'/tempImage/thumb/'.$newName;
+        // $image = $manager::make($sourcePath);
+        // $image->fit(300, 280);
+
+        // $image->save($destPath);
 
         return response()->json([
             'status' => true,
--- config/app.php
+++ config/app.php
@@ -165,10 +165,10 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        // Intervention\Image\ImageServiceProvider::class,
 
     ])->toArray(),
 
@@ -185,7 +185,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        // 'Image' => Intervention\Image\Facades\Image::class,
 
     ])->toArray(),
 
