51b7cadf6c83e562b01052fddf54fb682028c809
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-05-03 18:35:32 +0600

    filter index

--- app/Http/Controllers/frontend/ShopController.php
+++ app/Http/Controllers/frontend/ShopController.php
@@ -9,7 +9,7 @@ use App\Models\brand;
 use App\Models\Product;
 class ShopController extends Controller
 {
-    public function index ()
+    public function index (Request $request, $categorySlug = null, $subCategorySlug = null)
     {
         // not used, fetching from helper
         $categories = Category::orderBy('name', 'ASC')
@@ -20,18 +20,21 @@ class ShopController extends Controller
                 $brands = brand::orderBy('name', 'ASC')
                 ->where('status',1)
                 ->get();
-        
+
+        // apply filter
+        $produccts = Product::where('status', 1)->get();
+
         // used on shop.blade.php
         $products = Product::orderBy('id', 'DESC')
         ->where('status',1)
-        
+
                 ->get();
 
-        $data['categories'] = $categories;       
-        $data['brands'] = $brands;       
-        $data['products'] = $products;     
-        
-        
+        $data['categories'] = $categories;
+        $data['brands'] = $brands;
+        $data['products'] = $products;
+
+
         return view('frontend.shop', $data);
     }
 }
