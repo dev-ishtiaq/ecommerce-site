1c539047588a72cde886785645fe70652a9f25cf
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-05-18 01:05:09 +0600

    products table updated

--- database/migrations/2024_05_17_175432_alter_products_table.php
+++ database/migrations/2024_05_17_175432_alter_products_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema:table('products', function(Blueprint $table) {
+        Schema::table('products', function(Blueprint $table) {
             $table->text('short_description')->nullable()->after('description');
-            $table->text('short_description')->nullable()->after('description');
+            $table->text('shipping_returns')->nullable()->after('short_description');
+            $table->text('related_products')->nullable()->after('shipping_returns');
 
         });
     }
@@ -23,6 +24,11 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('products', function(Blueprint $table) {
+            $table->dropColumn('short_description');
+            $table->dropColumn('shipping_returns');
+            $table->dropColumn('related_products');
+
+        });
     }
 };
