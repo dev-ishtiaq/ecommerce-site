1e3818154f88c556796e7162f00bf6709aa722c9
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2024-06-23 23:56:40 +0600

    custom js

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -106,3 +106,20 @@
     </div>
 </section>
 @endsection
+@section("customJs")
+$('.add').click(function(){
+    var qtyElement = $(this).parent().prev(); // Qty Input
+    var qtyValue = parseInt(qtyElement.val());
+    if (qtyValue < 10) {
+        qtyElement.val(qtyValue+1);
+    }
+});
+
+$('.sub').click(function(){
+    var qtyElement = $(this).parent().next();
+    var qtyValue = parseInt(qtyElement.val());
+    if (qtyValue > 1) {
+        qtyElement.val(qtyValue-1);
+    }
+});
+@endsection
